8e13ea7591d2b97240c9fef767f9af0c0829cf01
LostButStillImproving <70686136+LostButStillImproving@users.noreply.github.com>
2020-12-27 16:42:07 +0100

    Did some mores

--- src/ChapterFive/Four/Fitbyte.java
+++ src/ChapterFive/Four/Fitbyte.java
@@ -5,12 +5,10 @@ public class Fitbyte {
 
     int age;
     int restingHeartRate;
-    double maximumHeartRate;
 
     public Fitbyte(int age, int restingHeartRate) {
         this.age = age;
         this.restingHeartRate = restingHeartRate;
-        this.maximumHeartRate = 206.3 - (0.711 * this.age);
     }
 
     public double targetHeartRate(double percentageOfMaximum) {
